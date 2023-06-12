360f120e485f8b747589d862d4a3c95973e05f92
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2023-06-12 17:01:07 -0500

    created multiple methods for working with nodes

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -21,12 +21,92 @@ public class LinkedList {
         length = 1;
     }
 
-//    Creates new Node
- public void append(int value){}
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    //    Creates new Node
+ public void append(int value){
+        Node newNode = new Node(value);
+        if (length == 0){ // if LinkedList is empty
+            head = newNode;
+            tail = newNode;
+        } else { //if list is not empty
+            tail.next = newNode; // moves new node to end of list
+            tail = newNode; // moves tail to new last node
+        }
+ }
+
+ public Node removeLastNode(){
+     if (length == 0) return null;// if length starts at 0 ;
+        Node pre = head;
+        Node temp = head;
+     while (temp.next != null){
+                pre = temp;
+                temp = temp.next;
+            }
+    tail = pre;
+    tail.next = null;
+    length--;
+    if (length == 0){ // for when a node is removed and length becomes 0
+        head = null;
+        tail = null;
+    }
+            return temp;
+        }
 //    Creates new Node, adds Node to end
- public void prepend(int value){}
+
+ public void prepend(int value){
+        Node newNode = new Node(value); //create new node
+        if (length == 0){ //if list empty, add new node
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode.next = head; //(node)=>  set value to previous first node
+            head = newNode; //then set head variable to new first node
+        }
+        length++;
+ }
 //    Creates new Node, adds Node to beginning
- public boolean insert(int index, int value){}
+
+    public Node removeFirstNode(){
+        if (length == 0) {return null;} // if list empty
+        Node temp = head;
+        head = head.next; //block of code used to remove node from beginning
+        temp.next = null;
+        length--;
+        if (length == 0){ // if removing only Node
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node getFromIndex(int index){
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+// public boolean insert(int index, int value){}
 //    Create new Node, insert Node at index
 
 }
--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -2,10 +2,52 @@ package LinkedLists;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList firstLinkedList = new LinkedList(4);
+        LinkedList firstLinkedList = new LinkedList(1);
+
+//        firstLinkedList.getHead(); // Head: 4
+//        firstLinkedList.getTail(); // Tail: 4
+//        firstLinkedList.getLength(); // Length: 4
+//
+//        firstLinkedList.printList(); // 4
 
         // head => Node(4)
         // tail ==//^
         //
+
+        firstLinkedList.append(2);
+
+        System.out.println("New List: ");
+        firstLinkedList.printList();
+//        REMOVE LAST NODE
+//        2 items in list
+//        System.out.println(firstLinkedList.removeLastNode().value);
+////        1 item in list
+//        System.out.println(firstLinkedList.removeLastNode());
+////        0 items in list
+//        System.out.println(firstLinkedList.removeLastNode());
+
+//        System.out.println("New List: ");
+//        firstLinkedList.append(3);
+//        firstLinkedList.printList();
+
+//        System.out.println(firstLinkedList.removeLastNode().value);
+//        System.out.println(firstLinkedList.removeLastNode());
+//        System.out.println("After remove last node: ");
+//        firstLinkedList.printList();
+
+//        firstLinkedList.prepend(5);
+//        System.out.println("New List: ");
+//        firstLinkedList.printList();
+
+//        System.out.println(firstLinkedList.removeFirstNode());
+//        System.out.println(firstLinkedList.removeFirstNode());
+//        System.out.println(firstLinkedList.removeFirstNode());
+
+        firstLinkedList.append(3);
+        firstLinkedList.append(4);
+        System.out.println("New List: ");
+        firstLinkedList.printList();
+
+        System.out.println(firstLinkedList.getFromIndex(2));
     }
 }
